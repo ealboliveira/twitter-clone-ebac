9fdf306c9bc7ada6fc84b1a188cf98b36a6aa4c1
Eduardo Oliveira <ealb.oliveira@gmail.com>
2024-04-24 13:36:18 -0300

    templantes

--- setup/settings.py
+++ setup/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "bookstore", "templantes")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
